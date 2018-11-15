060e55f9908b83b08f39c70ad8f984850393ae86
Lucas Rutten <luukie8000@gmail.com>
2018-11-15 13:52:51 +0100

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Kassa kassa = new Kassa();
+            kassa.OptieSelect();
+        }
+        
+
+        
+
+        
+
+    }
+}
